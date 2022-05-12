074b98107aca5913bbd8a8824ce25b8f3fbb8862
AhmedAtef277 <91344134+AhmedAtef277@users.noreply.github.com>
2022-05-12 22:58:29 +0200

    first commit

---
+++ TabBarController/ChatsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChatsViewController.swift
+//  TabBarController
+//
+//  Created by mac on 05/02/2022.
+//
+
+import UIKit
+
+class ChatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        tabBarController?.tabBar.isHidden = false
+
+    }
+    
+
+    
+
+}
---
+++ TabBarController/ContainerTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  ContainerTabBarController.swift
+//  TabBarController
+//
+//  Created by mac on 05/02/2022.
+//
+
+import UIKit
+
+class ContainerTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        selectedIndex = 1
+    }
+    
+
+  
+
+}
